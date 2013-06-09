52b05b9882e1635606e5a2ac5e8d0be773f0052c
supersheep <supersheepxjy@gmail.com>
2013-06-09 22:43:31 +0800

    complete neuro-events

---
+++ test/spec/neuro-events_test.js
@@ -0,0 +1,9 @@
+describe("neuro-events", function(){
+    describe("neuro-events.my_method()", function(){
+        it("should return 1", function(done){
+            NR.use(function() {
+                
+            });
+        });
+    });
+});
